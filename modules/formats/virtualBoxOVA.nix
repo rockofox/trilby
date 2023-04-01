5747ce440ae310ed14ef66f985e90e94a7edf0ab
rockofox <5049120+rockofox@users.noreply.github.com>
2023-04-01 20:40:54 +0200

    Update virtualBoxOVA.nix

--- modules/formats/virtualBoxOVA.nix
+++ modules/formats/virtualBoxOVA.nix
@@ -1,6 +1,7 @@
-{ inputs, ... }:
+{ inputs, lib, ... }:
 
 {
+  boot.loader.grub.device = lib.mkForce "/dev/sda";
   imports = [
     inputs.self.nixosModules.nixos.virtualisation.virtualbox-image
   ];
